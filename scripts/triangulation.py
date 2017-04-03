7e003e4974bfa406d815d7b43258e1e7129bea10
SeanCCarter <seancarter26@gmail.com>
2017-04-03 16:33:57 -0400

    added comments, removed unneccessary code.

--- scripts/triangulation.py
+++ scripts/triangulation.py
@@ -6,27 +6,25 @@ from sensor_msgs.msg import PointCloud
 import std_msgs
 
 
-def triangulate_opencv(pt_set1, pt_set2, P, P1):
-    pt_set1 = pt_set1.astype(float)
-    pt_set2 = pt_set2.astype(float)
-    my_points = cv2.triangulatePoints(P,P1,pt_set1.T,pt_set2.T)
-    projected_points_1 = P.dot(my_points)
+def triangulate(left_pts, right_pts, P, P1):
+    """ Takes in two sets of points, and two camera
+        projection matrices, to create a depth cloud
+
+        args:
+            pt_set1: list of points (x,y pairs) from left image
+            pt_set1: list of points (x,y pairs) from right image
+            P: camera projection for left image
+            P1: camera projection for right image
+
+        returns:
+            rospy pointcloud of points with x,y,z coordinates
+    """
+    left_pts = np.array(left_pts).astype(float)
+    right_pts = np.array(right_pts).astype(float)
+    my_points = cv2.triangulatePoints(P,P1,left_pts.T,right_pts.T)
 
     # convert to inhomogeneous coordinates
-    for i in range(projected_points_1.shape[1]):
-        projected_points_1[0,i] /= projected_points_1[2,i]
-        projected_points_1[1,i] /= projected_points_1[2,i]
-        projected_points_1[2,i] /= projected_points_1[2,i]
-
-    projected_points_2 = P1.dot(my_points)
-    # convert to inhomogeneous coordinates
-    for i in range(projected_points_2.shape[1]):
-        projected_points_2[0,i] /= projected_points_2[2,i]
-        projected_points_2[1,i] /= projected_points_2[2,i]
-        projected_points_2[2,i] /= projected_points_2[2,i]
-
-    # convert to inhomogeneous coordinates
-    for i in range(projected_points_2.shape[1]):
+    for i in range(my_points.shape[1]):
         my_points[0,i] /= my_points[3,i]
         my_points[1,i] /= my_points[3,i]
         my_points[2,i] /= my_points[3,i]
@@ -57,6 +55,6 @@ if __name__ == '__main__':
           [0.0, 494.6373026601465, 248.702205657959, 0.0],
           [0.0, 0.0, 1.0, 0.0]])
 
-    points1 = np.array([(376, 91), (361, 88), (485, 101), (459, 194), (380, 173)])
-    points2 = np.array([(212, 141), (197, 140), (313, 148), (297, 237), (221, 219)])
-    print(triangulate_opencv(points1, points2, p1, p2))
+    points1 = [(376, 91), (361, 88), (485, 101), (459, 194), (380, 173)]
+    points2 = [(212, 141), (197, 140), (313, 148), (297, 237), (221, 219)]
+    print(triangulate(points1, points2, p1, p2))
